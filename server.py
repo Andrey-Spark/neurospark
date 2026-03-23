652d28f054eff684a05dfd6d76bb2e5679795faf
Andrey Spark <andre.kad@yandex.ru>
2026-03-23 23:50:01 +0300

    fix cors origins

--- server.py
+++ server.py
@@ -8,7 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # фронт React
+    allow_origins=[
+        "http://localhost:3000",
+        "https://spiffy-beignet-c2dfdc.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
